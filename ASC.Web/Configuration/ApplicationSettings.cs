95d7301125b4c71f6893b8b1cf55272f468d96d1
Lac To Minh <lacminh161@gmail.com>
2026-03-21 10:26:24 +0700

    Add project files.

---
+++ ASC.Web/Configuration/ApplicationSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace ASC.Web.Configuration
+{
+  public class ApplicationSettings
+  {
+    public String ApplicationTitle { get; set; }
+  }
+}
---
+++ ASC.Web/Services/AuthMessageSender.cs
@@ -0,0 +1,17 @@
+﻿namespace ASC.Web.Services
+{
+  public class AuthMessageSender : IEmailSender, ISmsSender
+  {
+    public Task SendEmailAsync(string email, string subject, string message)
+    {
+      // Code thực tế gọi API gửi email sẽ nằm ở đây
+      return Task.FromResult(0);
+    }
+
+    public Task SendSmsAsync(string number, string message)
+    {
+      // Code thực tế gọi API gửi SMS sẽ nằm ở đây
+      return Task.FromResult(0);
+    }
+  }
+}
